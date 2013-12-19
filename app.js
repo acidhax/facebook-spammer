375f37f69cf507b94ee5efc8c772574320c5c73d
Mathieu Gosbee <mail@matbee.com>
2013-12-19 14:38:06 -0500

    It works, it works! Wait, fuck. Facebook marked me as spam.

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 	},
 	function(accessToken, refreshToken, profile, done) {
 		getFacebookFriends(accessToken, function (err, friends) {
-			console.log(err, friends);
 			usersFriends[profile.id] = friends;
-			itsgotime(profile, friends);
-		done(null, profile);
+			console.log("itsgotime();");
+			itsgotime(profile, friends, "hey.. do you or anyone you know watch like TV or movies online together? Like.. watch the same thing and talk over skype/phone or text?", accessToken);
+			done(null, profile);
 		});
 	}
 ));
@@ -76,13 +76,11 @@ app.get('/auth/facebook/callback',
 	})
 );
 passport.serializeUser(function(user, done) {
-	console.log("serialize", user);
 	users[user.id] = user;
  	done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-	console.log("deserialize", users[id]);
 	done(null, users[id]);
 });
 
@@ -105,25 +103,39 @@ http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
 
-var itsgotime = function (profile, friends) {
-
+var itsgotime = function (profile, friends, message, accessToken) {
+	console.log("It's go time:", profile, friends.data.length, message);
+	var ready = false;
+	async.forEachSeries(friends.data, function (friend, next) {
+		console.log("Delaying each friend");
+		setTimeout(function () {
+			sendMessage(profile.id, friend.id, message, accessToken, next);
+		}, 10000);
+	}, function (err) {
+		// complete.
+	});
 }
 
 var connections = {};
-var sendMessage = function (senderid, receiverid, accessToken, message, cb) {
+var sendMessage = function (senderid, receiverid, message, accessToken, cb) {
+	console.log("sendMessage", senderid, receiverid, message, accessToken)
 	var params = {
 		facebookId : senderid,
-		secret_key : facebookSecretKey,
+		appSecret : facebookSecretKey,
 		appId : facebookAppId,
-		accessToken : accessToken
+		accessToken : accessToken,
+		host: 'chat.facebook.com'
 	};
 	if (!connections[senderid]) {
+		console.log("Creating facebook client.");
 		var facebookClient = new facebookChat(params);
 		connections[senderid] = {ready: false, client: facebookClient};
 		facebookClient.on('online', function () {
 			console.log("ONLINE!", senderid);
-			connections[senderid].ready = true;
-			ready(cb);
+			if (connections[senderid].ready === false) {
+				connections[senderid].ready = true;
+				ready(cb);
+			}
 		});
 		facebookClient.on('message', function(message){
 			console.log(message);
@@ -132,22 +144,25 @@ var sendMessage = function (senderid, receiverid, accessToken, message, cb) {
 		facebookClient.on('composing', function(from){
 			console.log(from + ' is composing a message');
 		});
-		facebookClient.on('error', function () {
+		facebookClient.on('error', function (err) {
 			if (err == 'XMPP authentication failure') {
 				// Yo what?
 				delete connections[senderid];
 			}
+			console.log("errrr", err);
 		});
 	} else {
 		ready(cb);
 	}
 	
 	function ready(cb) {
-		_sendMessage(connections[senderid], receiverId, message);
+		console.log("Ready");
+		_sendMessage(connections[senderid].client, receiverid, message);
 		cb && cb();
 	}
 }
 
-var _sendMessage = function (client, receiverId, message) {
-	client.send('-'+receiverId+'@chat.facebook.com', message);
+var _sendMessage = function (client, receiverid, message) {
+	client.send('-'+receiverid+'@chat.facebook.com', message);
+	console.log("Sending message to", receiverid, message);
 }
